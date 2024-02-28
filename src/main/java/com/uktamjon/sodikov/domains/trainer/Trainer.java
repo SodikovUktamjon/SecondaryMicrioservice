2cada2e082c2aacc6fb84917d628fdf6c6a24b4e
Uktamjon.Sodikov <sodikovuktamjon30@gmail.com>
2024-02-28 11:05:28 +0500

    Services and domains test

--- src/main/java/com/uktamjon/sodikov/domains/trainer/Trainer.java
+++ src/main/java/com/uktamjon/sodikov/domains/trainer/Trainer.java
@@ -1,22 +1,32 @@
 package com.uktamjon.sodikov.domains.trainer;
 
 import com.uktamjon.sodikov.enums.ActionType;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Trainer {
-    private int id;
-    private String username;
+
+    @Id
+    private Integer id;
+
     private String firstName;
     private String lastName;
+    private String username;
     private boolean isActive;
-    private LocalDateTime startDate;
+    @Transient
     private int duration;
+    @Transient
+    private LocalDateTime startDate;
+
+    @Transient
     private ActionType actionType;
 }
